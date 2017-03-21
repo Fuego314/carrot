51239a98b14d05174f1e34cadae5ded15ff19024
Fuego314 <l.tucker@hotmail.co.uk>
2017-03-21 18:07:13 +0000

    Start contact form and add 'find out more' section

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,60 @@
-$('.types-slider').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 590,
-      settings: {
-        arrows: true,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+$(function() {
+  "use strict";
 
-// TYPES SECTION
-function hideInfo () {
-  $('.type-info').removeClass('open-info');
-}
+  // NAV
+  function navToggle() {
+    $('.navbar-toggle').toggleClass('navbar-toggle-open');
+    $('nav').toggleClass('open');
+  }
 
-// Show carrot info on info icon click (only active carrot)
-$('.info-icon').click(function () {
-  if ($(this).closest('.carrot-type').hasClass('slick-active')) $('.slick-center .type-info').addClass('open-info');
+  // Show nav toggle animation and open close menu on click
+  $('.navbar-toggle').unbind().click(() => navToggle());
+
+  // HEADER
+  $('.carrot-harvest').click(function () {
+    $(this).addClass('pull-carrot')
+  });
+
+  // TYPES SECTION
+  function hideInfo () {
+    $('.type-info').removeClass('open-info');
+  }
+
+  // Init slider
+  $('.types-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  // Show carrot info on info icon click (only active carrot)
+  $('.info-icon').click(function () {
+    if ($(this).closest('.carrot-type').hasClass('slick-active')) $('.slick-center .type-info').addClass('open-info');
+  });
+  // Hide carrot info on close icon click
+  $('.type-info img').click(() => hideInfo() );
+  // Hide carrot info on arrow click and swipe
+  $('.types-slider').on('beforeChange', (e) => hideInfo() );
+
+
 });
-// Hide carrot info on close icon click
-$('.type-info img').click(() => hideInfo() );
-// Hide carrot info on arrow click and swipe
-$('.types-slider').on('beforeChange', (e) => hideInfo() );
