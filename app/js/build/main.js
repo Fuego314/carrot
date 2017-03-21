0f7c8a95612024e0602bef964f6eaeaee035da5d
Fuego314 <l.tucker@hotmail.co.uk>
2017-03-21 14:31:14 +0000

    Style 'Types' section for mobile and add slick slider

--- app/js/build/main.js
+++ app/js/build/main.js
@@ -1,1 +1,42 @@
-"use strict";
+'use strict';
+
+$('.types-slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '20px',
+      slidesToShow: 3
+    }
+  }, {
+    breakpoint: 590,
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1
+    }
+  }]
+});
+
+// TYPES SECTION
+function hideInfo() {
+  $('.type-info').removeClass('open-info');
+}
+
+// Show carrot info on info icon click (only active carrot)
+$('.info-icon').click(function () {
+  if ($(this).closest('.carrot-type').hasClass('slick-active')) $('.slick-center .type-info').addClass('open-info');
+});
+// Hide carrot info on close icon click
+$('.type-info img').click(function () {
+  return hideInfo();
+});
+// Hide carrot info on arrow click and swipe
+$('.types-slider').on('beforeChange', function (e) {
+  return hideInfo();
+});
